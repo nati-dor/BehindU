cd5e95d9e217b577f34ac71987a4e55551b068a5
Nati Dor <netanelduk@gmail.com>
2020-05-14 11:43:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/behindu/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.behindu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button forgotPassBtn = (Button) findViewById(R.id.forgot_pass_btn);
+        Button newUserBtn = (Button) findViewById(R.id.new_user_btn);
+
+        forgotPassBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForgotPassword.class);
+                startActivity(intent);
+            }
+        });
+
+        newUserBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+       Intent intent = new Intent(MainActivity.this,Registration.class);
+       startActivity(intent);
+            }
+        });
+    }
+}
+
+
---
+++ app/src/main/java/com/example/behindu/SelectAnAction.java
@@ -0,0 +1,14 @@
+package com.example.behindu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectAnAction extends AppCompatActivity{
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.select_an_action);
+        }
+}
